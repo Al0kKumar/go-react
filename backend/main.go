54f4215f3ef98567709786e24ab58264e6a4edb8
Alok <mishraalok189381@gmail.com>
2024-10-28 21:25:09 +0530

    done with first section

--- backend/main.go
+++ backend/main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 type Todo struct {
-	ID          int     `json:"id"` 
-	Completed   bool     `json:"completed"`
-	Body        string   `json:"body"`
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
 }
-
 
 var todos []Todo
 
@@ -21,20 +22,29 @@ func main() {
 
 	app := fiber.New()
 
+	err := godotenv.Load(".env")
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(fiber.Map{"msg":"hello world"})
+	if err != nil {
+		log.Fatal("Error handling env file")
+	}
+
+	PORT := os.Getenv("PORT")
+
+	// get all todos
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(todos)
 	})
 
+	// create a todo
 	app.Post("/api/todos", func(c *fiber.Ctx) error {
 		todo := &Todo{}
 
-		if err := c.BodyParser(todo); err != nil{
+		if err := c.BodyParser(todo); err != nil {
 			return err
 		}
 
-		if todo.Body == ""{
-			return c.Status(400).JSON(fiber.Map{"error":"todo body is reqiured"})
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "todo body is reqiured"})
 		}
 
 		todo.ID = len(todos) + 1
@@ -43,5 +53,34 @@ func main() {
 		return c.Status(201).JSON(todo)
 	})
 
-	log.Fatal(app.Listen(":3000"))
+	//update a todo
+	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, todo := range todos {
+			if fmt.Sprint(todo.ID) == id {
+				todos[i].Completed = true
+				return c.Status(200).JSON(todos[i])
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{"error": "todo not found"})
+
+	})
+
+	// delete a todo
+	app.Delete("api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, todo := range todos {
+			if fmt.Sprint(todo.ID) == id {
+				todos = append(todos[:i], todos[i+1:]...)
+				return c.Status(200).JSON(fiber.Map{"success": true})
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{"error": "todo not found"})
+	})
+
+	log.Fatal(app.Listen(":" + PORT))
 }
